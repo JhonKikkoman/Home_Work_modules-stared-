733abc8fd69c3cf81c9c31e31cfdb7b7f0db9ad3
Piskun_Ivan <tw1st3r111@mail.ru>
2023-06-13 23:49:30 +0300

    task2

---
+++ app.js
@@ -0,0 +1,7 @@
+import "./domain.js"
+import "./game.js"
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from "./game.js";
+
+const game = new Game();
+
+game.start();
